7dae6fcb94ce95e60943979c4085c2990f79c83c
Dawn <1401629898@qq.com>
2019-08-05 16:23:25 +0800

    判断对象是否为空

--- handleObject.js
+++ handleObject.js
@@ -1,1 +1,14 @@
-
+const handleObject = {
+  /**
+   * @description 判断对象是否为空
+   * @param {Object} obj 待判断对象
+   * @returns {Boolean} true && false
+   */
+  isEmpty: obj => {
+    let t;
+    for (t in obj) {
+      return false;
+    }
+    return true;
+  },
+}
